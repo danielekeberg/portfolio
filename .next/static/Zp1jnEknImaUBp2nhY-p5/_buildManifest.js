4e478d2417d803fe456b56c346233040eb017071
Daniel Lehre Ekeberg <danieleke@live.no>
2024-09-29 02:53:12 +0200

    npm install next react react-dom

---
+++ .next/static/Zp1jnEknImaUBp2nhY-p5/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-77823ddac6993d35.js"],"/[slug]":["static/chunks/pages/[slug]-35f4835d8b340e27.js"],sortedPages:["/_app","/_error","/[slug]"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
