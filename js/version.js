603a63b33f4ad0efb041acb136fd6aac407150f7
Daniel Lehre Ekeberg <danieleke@live.no>
2024-10-17 12:10:26 +0200

    Update version.js

--- js/version.js
+++ js/version.js
@@ -5,3 +5,12 @@ fetch('../package.json')
     document.getElementById('version').innerText = `Version: ${data.version}`;
   })
   .catch(error => console.error('Error fetching version:', error));
+
+// fetch('../package.json')  
+//   .then(response => response.json())
+//   .then(data => {
+//     const versionElements = document.querySelectorAll('.version');
+//     versionElements.forEach(element => {
+//       element.textContent = data.version;
+//     });
+//   });
