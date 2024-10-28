4408c894e8db5ccf0368fdc70644726836c3051f
Daniel Lehre Ekeberg <danieleke@live.no>
2024-10-28 15:40:17 +0100

    Updated Dice code

--- js/dice.js
+++ js/dice.js
@@ -13,54 +13,73 @@ let diceP = document.getElementById("diceP");
 function dice() {
     const diceNumber = Math.floor(Math.random() * 6) + 1;
     console.log(diceNumber);
-    if (diceNumber === 1) {
-        diceOne.style.display = "flex";
-        diceTwo.style.display = "none";
-        diceThree.style.display = "none";
-        diceFour.style.display = "none";
-        diceFive.style.display = "none";
-        diceSix.style.display = "none";
-    } 
-    if (diceNumber === 2) {
-        diceOne.style.display = "none";
-        diceTwo.style.display = "flex";
-        diceThree.style.display = "none";
-        diceFour.style.display = "none";
-        diceFive.style.display = "none";
-        diceSix.style.display = "none";
+    if(diceNumber === 1) {
+        diceOne.src = "../src/dice-six-faces-one.png"
     }
-    if (diceNumber === 3) {
-        diceOne.style.display = "none";
-        diceTwo.style.display = "none";
-        diceThree.style.display = "flex";
-        diceFour.style.display = "none";
-        diceFive.style.display = "none";
-        diceSix.style.display = "none";
+    if(diceNumber === 2) {
+        diceOne.src = "../src/dice-six-faces-two.png"
     }
-    if (diceNumber === 4) {
-        diceOne.style.display = "none";
-        diceTwo.style.display = "none";
-        diceThree.style.display = "none";
-        diceFour.style.display = "flex";
-        diceFive.style.display = "none";
-        diceSix.style.display = "none";
+    if(diceNumber === 3) {
+        diceOne.src = "../src/dice-six-faces-three.png"
     }
-    if (diceNumber === 5) {
-        diceOne.style.display = "none";
-        diceTwo.style.display = "none";
-        diceThree.style.display = "none";
-        diceFour.style.display = "none";
-        diceFive.style.display = "flex";
-        diceSix.style.display = "none";
+    if(diceNumber === 4) {
+        diceOne.src = "../src/dice-six-faces-four.png"
     }
-    if (diceNumber === 6) {
-        diceOne.style.display = "none";
-        diceTwo.style.display = "none";
-        diceThree.style.display = "none";
-        diceFour.style.display = "none";
-        diceFive.style.display = "none";
-        diceSix.style.display = "flex";
+    if(diceNumber === 5) {
+        diceOne.src = "../src/dice-six-faces-five.png"
     }
+    if(diceNumber === 6) {
+        diceOne.src = "../src/dice-six-faces-six.png"
+    }
+
+    // if (diceNumber === 1) {
+    //     diceOne.style.display = "flex";
+    //     diceTwo.style.display = "none";
+    //     diceThree.style.display = "none";
+    //     diceFour.style.display = "none";
+    //     diceFive.style.display = "none";
+    //     diceSix.style.display = "none";
+    // } 
+    // if (diceNumber === 2) {
+    //     diceOne.style.display = "none";
+    //     diceTwo.style.display = "flex";
+    //     diceThree.style.display = "none";
+    //     diceFour.style.display = "none";
+    //     diceFive.style.display = "none";
+    //     diceSix.style.display = "none";
+    // }
+    // if (diceNumber === 3) {
+    //     diceOne.style.display = "none";
+    //     diceTwo.style.display = "none";
+    //     diceThree.style.display = "flex";
+    //     diceFour.style.display = "none";
+    //     diceFive.style.display = "none";
+    //     diceSix.style.display = "none";
+    // }
+    // if (diceNumber === 4) {
+    //     diceOne.style.display = "none";
+    //     diceTwo.style.display = "none";
+    //     diceThree.style.display = "none";
+    //     diceFour.style.display = "flex";
+    //     diceFive.style.display = "none";
+    //     diceSix.style.display = "none";
+    // }
+    // if (diceNumber === 5) {
+    //     diceOne.style.display = "none";
+    //     diceTwo.style.display = "none";
+    //     diceThree.style.display = "none";
+    //     diceFour.style.display = "none";
+    //     diceFive.style.display = "flex";
+    //     diceSix.style.display = "none";
+    // }
+    // if (diceNumber === 6) {
+    //     diceOne.style.display = "none";
+    //     diceTwo.style.display = "none";
+    //     diceThree.style.display = "none";
+    //     diceFour.style.display = "none";
+    //     diceFive.style.display = "none";
+    //     diceSix.style.display = "flex";
+    // }
 }
 
 diceButton.addEventListener("click", dice);
