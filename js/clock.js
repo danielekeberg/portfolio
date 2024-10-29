6b7e1dbc67dd8e5886ab58d88fbd87fba4620a43
Daniel Lehre Ekeberg <danieleke@live.no>
2024-10-30 00:54:56 +0100

    Create clock.js

---
+++ js/clock.js
@@ -0,0 +1,15 @@
+const displayClock = document.getElementById("digitalClock");
+
+function updateClock() {
+    const now = new Date();
+    const hours = String(now.getHours()).padStart(2, '0');
+    const minutes = String(now.getMinutes()).padStart(2, '0');
+    const seconds = String(now.getSeconds()).padStart(2, '0');
+
+    const timeString = `${hours}:${minutes}:${seconds}`;
+    displayClock.textContent = timeString;
+}
+
+setInterval(updateClock, 1000);
+
+updateClock();
