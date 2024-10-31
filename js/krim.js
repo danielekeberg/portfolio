5d56d17da83da2ba64c8e1b9bae023d4fc8e38bc
Daniel Lehre Ekeberg <danieleke@live.no>
2024-10-31 05:15:32 +0100

    Update krim.js

--- js/krim.js
+++ js/krim.js
@@ -1,15 +1,14 @@
 const hotell = document.getElementById("hotell");
-const book = document.getElementById("inHotell");
+// const book = document.getElementById("inHotell");
 const krims = document.getElementById("krim");
 const crimes = document.querySelector(".crimes");
 const krim = document.querySelectorAll(".krim");
 const krimTekst = document.getElementById("krimTekst");
 
 let isInHotell = true;
+let previousIndex = -1;
 
-const stjelPenger = Math.floor(Math.random() * (900 - 500)) + 500;
-const ranAutomat = Math.floor(Math.random() * (2000 - 500)) + 500;
-const deliPenger = Math.floor(Math.random() * (10000 - 5000)) + 5000;
+
 
 
 // hotellStatus();
@@ -65,21 +64,35 @@ function krimUten() {
 }
 
 function enkelKrim() {
+    const miniPenger = Math.floor(Math.random() * (900 - 500)) + 500;
+    const mediumPenger = Math.floor(Math.random() * (2000 - 500)) + 500;
+    const storePenger = Math.floor(Math.random() * (10000 - 5000)) + 5000;
+
     const krimPre = [
-        `Du stjeler ${stjelPenger} kroner fra en gammel dame.`,
-        `Du raner en brus automat og får tak i ${ranAutomat} kroner.`,
-        `Du raner Deli de Luca og tar med deg ${deliPenger} kroner.`,
-        `Politiet så deg pisse i gaten. Du havner i fengsel og du får en bot på 5000 kroner.`,
-        `Du faller og brekker armen.`,
-        `Jeg har ikke flere forslag.`,
-        `Mannen i kassen stanser deg.`
+        `Du stjeler ${miniPenger} kroner fra en gammel dame.`,
+        `Du raner en brus automat og får tak i ${mediumPenger} kroner.`,
+        `Du raner Deli de Luca og tar med deg ${storePenger} kroner.`,
+        `Du blir stoppet av politiet og får en bot på ${mediumPenger} kroner.`,
+        `Du faller og brekker armen. Du betaler ${miniPenger} på sykehuset.`,
+        `Jeg har ikke flere forslag, men her får du ${storePenger} kroner.`,
+        `Mannen i kassen stanser deg og du gir tilbake de ${miniPenger} kronene du tok.`
     ];
+
+
+
     if(isInHotell) {
         krimTekst.innerText = "Du må ut av hotell for å gjøre denne handlingen!";
     } else {
-        const randomTall = Math.floor(Math.random() * krimPre.length);
-        
+        let randomTall;
 
+        do {
+            randomTall = Math.floor(Math.random() * krimPre.length);
+        } while (randomTall === previousIndex);
+
+        previousIndex = randomTall;
+
+        // const randomTall = Math.floor(Math.random() * krimPre.length);
+        
         const krimTekstNummer = randomTall + 1;
         krimTekst.textContent = `${krimPre[(randomTall)]}`
         console.log("Krimtekst nummer: " + krimTekstNummer);
