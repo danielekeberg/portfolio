c0a57c65d962ea3655290e3b82ec8c5f4749eb5d
Daniel Lehre Ekeberg <danieleke@live.no>
2024-11-09 07:09:19 +0100

    Update roulette.js

--- js/roulette.js
+++ js/roulette.js
@@ -1,35 +1,48 @@
 const cashMoney = document.getElementById("cashMoney");
 const rouletteDisplay = document.getElementById("rouletteDisplay")
+const rouletteScreen = document.getElementById("displayScreen");
 const submitRoulette = document.getElementById("submitRoulette");
 
 let totalPenger = 1000000;
 
 function roulette() {
     const inputNumber = document.getElementById("inputNumber");
-    const rouletteInput = inputNumber.value;
-    const rouletteMath = Math.floor(Math.random() * 37);
+    const rouletteInput = parseInt(inputNumber.value, 10);
+    const rouletteMath = Math.floor(Math.random() * 39);
+    rouletteDisplay.style.color = "#333";
+    rouletteDisplay.style.backgroundColor = "#acacac";
+    rouletteScreen.style.backgroundColor = "#acacac";
 
-    if (pengeCounter === 0) {
+    if (pengeCounter <= 0) {
         rouletteDisplay.style.color = "#ad5151";
         rouletteDisplay.style.fontSize = "18px";
         rouletteDisplay.textContent = "Du har ikke nok penger";
     } else {
-        if (rouletteMath == rouletteInput) {
-            pengeCounter += 3500000;
-            localStorage.setItem("pengeCounter", pengeCounter);
-            rouletteDisplay.textContent = `Hjulet landet på: ${rouletteMath}`;
-            pengeCounterP.textContent = `Penger: ${pengeCounter} kr`;
-            cashMoney.textContent = `Penger: ${pengeCounter}`;
-            console.log(rouletteMath);
-            console.log("Du vinner");
+        if (rouletteInput > 38 || rouletteInput < 0 || isNaN(rouletteInput)) {
+            rouletteDisplay.style.color = "#ad5151";
+            rouletteDisplay.style.fontSize = "20px";
+            rouletteDisplay.textContent = `Velg et tall mellom 0-38`;
         } else {
-            pengeCounter -= 100000;
-            localStorage.setItem("pengeCounter", pengeCounter);
-            rouletteDisplay.textContent = `Hjulet landet på: ${rouletteMath}`;
-            pengeCounterP.textContent = `Penger: ${pengeCounter} kr`;
-            cashMoney.textContent = `Penger: ${pengeCounter}`;
-            console.log(rouletteMath);
-            console.log("Du taper");
+            if (rouletteMath == rouletteInput) {
+                pengeCounter += 3500000;
+                localStorage.setItem("pengeCounter", pengeCounter);
+                rouletteScreen.style.backgroundColor = "#a1d99b";
+                rouletteDisplay.style.backgroundColor = "#a1d99b";
+                rouletteDisplay.textContent = `Hjulet landet på: ${rouletteMath}`;
+                pengeCounterP.textContent = `Penger: ${pengeCounter} kr`;
+                cashMoney.textContent = `Penger: ${pengeCounter}`;
+                console.log(rouletteMath);
+                console.log("Du vinner");
+            } else {
+                pengeCounter -= 100000;
+                localStorage.setItem("pengeCounter", pengeCounter);
+                rouletteDisplay.textContent = `Hjulet landet på: ${rouletteMath}`;
+                pengeCounterP.textContent = `Penger: ${pengeCounter} kr`;
+                cashMoney.textContent = `Penger: ${pengeCounter}`;
+                console.log(rouletteMath);
+                console.log("Du taper");
+                console.log(rouletteMath.length);
+            }
         }
     }
 }
