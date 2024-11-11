731e28183b102e148ea365f33b9c191bf318d2ba
Daniel Lehre Ekeberg <danieleke@live.no>
2024-11-11 10:58:29 +0100

    Update greeting.js

--- js/greeting.js
+++ js/greeting.js
@@ -1,10 +1,16 @@
 const nameDisplay = document.getElementById("nameDisplay");
 const nameButton = document.getElementById("nameSubmit");
+const bannedWords = ["hello", "hade", "bannedword", "test"];
 
 function greeting() {
     const username = document.getElementById("nameInput").value;
     const userColor = document.getElementById("colorInput").value;
-    nameDisplay.innerHTML = `<p>Welcome <span style="color: ${userColor}">${username}</span>!</p>`;
+    if (bannedWords.includes(username)) {
+        nameDisplay.textContent = `That word is not allowed`;
+    } else {
+        nameDisplay.innerHTML = `<p>Welcome <span style="color: ${userColor}">${username}</span>!</p>`;
+    }
+    
 }
 
 nameButton.addEventListener("click", greeting);
