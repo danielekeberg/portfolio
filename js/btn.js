717879cbba2a3dd37fb91b31ab5039a75fdc536c
Daniel Lehre Ekeberg <154937462+danielekeberg@users.noreply.github.com>
2024-11-14 14:32:51 +0100

    Update btn.js

--- js/btn.js
+++ js/btn.js
@@ -1,4 +1,10 @@
 const btnClr = document.getElementById("btnColor");
+const hexCheck = document.getElementById("hexCheck");
+const hexLabel = document.getElementById("hexLabel");
+const btnColor = document.getElementById("btnColorInput");
+const btnTxtColor = document.getElementById("btnTxtColorInput");
+const btnColorInputHex = document.getElementById("btnColorInputHex");
+const btnTxtColorInputHex = document.getElementById("btnTxtColorInputHex");
 
 function changeBtnClr() {
     const clrInput = document.getElementById("btnColorInput").value;
@@ -7,4 +13,36 @@ function changeBtnClr() {
     btnClr.style.color = `${txtClrInput}`;
 }
 
+function changeBtnClrHex() {
+    const clrTxtInputHex = document.getElementById("btnTxtColorInputHex").value;
+    const clrInputHex = document.getElementById("btnColorInputHex").value;
+    btnClr.style.background = `#${clrInputHex}`;
+    btnClr.style.color = `#${clrTxtInputHex}`;
+}
+
 btnClr.addEventListener("click", changeBtnClr);
+btnClr.addEventListener("click", changeBtnClrHex);
+hexCheck.addEventListener("change", swapHex);
+
+function swapHex() {
+    if(hexCheck.checked) {
+        btnColor.style.display = "none";
+        btnTxtColor.style.display = "none";
+        btnColorInputHex.style.display = "flex";
+        btnTxtColorInputHex.style.display = "flex";
+    } else {
+        btnColorInputHex.style.display = "none";
+        btnTxtColorInputHex.style.display = "none";
+        btnColor.style.display = "flex";
+        btnTxtColor.style.display = "flex";
+    }
+}
+
+function loadInput() {
+    btnColorInputHex.style.display = "none";
+    btnTxtColorInputHex.style.display = "none";
+    btnColor.style.display = "flex";
+    btnTxtColor.style.display = "flex";
+}
+
+loadInput();
