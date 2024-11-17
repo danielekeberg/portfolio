318859e6a489ef9c2ed8b59d85dea3df80cbffad
Daniel Lehre Ekeberg <danieleke@live.no>
2024-11-17 16:33:21 +0100

    Update btn.js

--- js/btn.js
+++ js/btn.js
@@ -213,3 +213,17 @@ function dropRedirect() {
 link1.addEventListener("click", dropImg);
 link2.addEventListener("click", dropdownH3);
 link3.addEventListener("click", dropRedirect);
+
+const numbers = [1, 2, 3, 4, 5];
+const squares = numbers.map(square);
+const cubes = numbers.map(cube);
+
+console.log(cubes);
+
+function square(element) {
+    return Math.pow(element, 2)
+}
+
+function cube(element) {
+    return Math.pow(element, 3);
+}
