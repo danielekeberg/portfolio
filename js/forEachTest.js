8e4ae4d96068a9d90b60da5b34d682e6850dabcb
Daniel Lehre Ekeberg <154937462+danielekeberg@users.noreply.github.com>
2024-12-22 22:22:45 +0100

    Update forEachTest.js

--- js/forEachTest.js
+++ js/forEachTest.js
@@ -1,10 +1,19 @@
 const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 13, 52, 24, 33, 100, 73, 55];
+const names = ["Daniel", "Therese", "Hilde", "Håvard", "Romeo"]
+const forEachDiv = document.createElement("div");
 
 numbers.sort((a, b) => a - b);
+names.sort((a, b) => a - b);
 
 numbers.forEach(number => {
-    const forEachDiv = document.createElement("div");
     forEachDiv.classList.add("forEachDivContainer");
     forEachDiv.innerHTML = `This is number ${number}`;
     document.getElementById("forEachContainer").appendChild(forEachDiv);
 });
+
+names.forEach(name => {
+    forEachDiv.classList.add("forEachDivContainer");
+    forEachDiv.innerHTML = `${name}`;
+    document.getElementById("forEachContainer").appendChild(forEachDiv);
+});
+    
