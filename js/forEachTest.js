f7cd54102d513b371bcedfc8ded87d1dd11960bf
Daniel Lehre Ekeberg <154937462+danielekeberg@users.noreply.github.com>
2024-12-22 18:06:42 +0100

    Create forEachTest.js

---
+++ js/forEachTest.js
@@ -0,0 +1,10 @@
+const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 13, 52, 24, 33, 100, 73, 55];
+
+numbers.sort((a, b) => a - b);
+
+numbers.forEach(number => {
+    const forEachDiv = document.createElement("div");
+    forEachDiv.classList.add("forEachDivContainer");
+    forEachDiv.innerHTML = `This is number ${number}`;
+    document.getElementById("forEachContainer").appendChild(forEachDiv);
+});
