61e6a146691a7e2df2b250f8ce2cd34b635954be
Daniel Lehre Ekeberg <154937462+danielekeberg@users.noreply.github.com>
2024-12-22 16:02:10 +0100

    Update christmasCountdown.js

--- js/christmasCountdown.js
+++ js/christmasCountdown.js
@@ -23,6 +23,31 @@ function updateCountdown() {
     }
 }
 
+function updateBirthdayCountdown() {
+    const birthday = new Date('April 8, 2025 00:00:00').getTime();
+    const now = new Date().getTime();
+    const birthdayTimeLeft = birthday - now;
+
+    const days = Math.floor(birthdayTimeLeft / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((birthdayTimeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((birthdayTimeLeft % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((birthdayTimeLeft % (1000 * 60)) / 1000);
+
+    document.getElementById("birthdayDays").textContent = days;
+    document.getElementById("birthdayHours").textContent = hours;
+    document.getElementById("birthdayMinutes").textContent = minutes;
+    document.getElementById("birthdaySeconds").textContent = seconds;
+
+    if (birthdayTimeLeft < 0) {
+        clearInterval(birthdayTimer);
+        document.getElementById("countdown").style.display = "block";
+        document.getElementById("countdown").innerHTML = `
+        <h3>Happy Birthday</h3>
+        `;
+    }
+}
+
 const timer = setInterval(updateCountdown, 1000);
+const birthdayTimer = setInterval(updateBirthdayCountdown, 1000);
 
 updateCountdown();
