e0a87461ca0d54b63466ef8519cf84047c07ee5f
Daniel Lehre Ekeberg <154937462+danielekeberg@users.noreply.github.com>
2025-01-26 19:37:26 +0100

    Create /post/index.html
    User can now click a specific breed to only fetch images from that breed

--- dog/js/breed.js
+++ dog/js/breed.js
@@ -1,8 +1,11 @@
 const apiUrl = 'https://dog.ceo/api/breeds';
+const params = new URLSearchParams(window.location.search);
+const searchedBreed = params.get('breed');
+const uppercaseBreed = searchedBreed.toUpperCase();
 
 async function fetchDog() {
     try {
-        const response = await fetch(`${apiUrl}/image/random`);
+        const response = await fetch(`https://dog.ceo/api/breed/${searchedBreed}/images/random`);
 
         if (!response.ok) {
             throw new Error('Error fetching dog image');
@@ -10,7 +13,7 @@ async function fetchDog() {
 
         const data = await response.json();
         document.getElementById('randomDog').src = `${data.message}`;
-
+        document.getElementById('newDog').textContent = `Fetch ${uppercaseBreed} Images`;
         
     }
     catch(error) {
@@ -39,7 +42,7 @@ async function fetchBreeds() {
         Object.keys(breeds).forEach(breed => {
             const d = document.createElement('div');
             d.className = 'dog';
-            d.innerHTML = `<a href="./post/?breed=${breed}"><p>${breed}</p></a>`;
+            d.innerHTML = `<a href="./?breed=${breed}"><p>${breed}</p></a>`;
             allBreeds.appendChild(d);
         })
     }
