ffc610240e072ff2723d3072eb3eecbdf85beeb0
Daniel Lehre Ekeberg <154937462+danielekeberg@users.noreply.github.com>
2025-02-05 07:27:24 +0100

    Created RNG with history display

---
+++ rng/js/script.js
@@ -0,0 +1,30 @@
+function rng() {
+    const max = document.getElementById('max').value.trim();
+    const min = document.getElementById('min').value.trim();
+    const maxNum = Number(max);
+    const minNum = Number(min);
+    const random = Math.floor(Math.random() * ((maxNum + 1) - minNum)) + minNum;
+
+    document.getElementById('display').textContent = random;
+
+
+    const p = document.createElement('p');
+    p.textContent = `You've generated a random number between ` + min + ` and ` + max + `!`;
+    const p2 = document.createElement('p');
+    p2.textContent = `The result was: ${random}`;
+
+    document.getElementById('history').appendChild(p);
+    document.getElementById('history').appendChild(p2);
+}
+
+document.getElementById('submit').addEventListener('click', rng);
+document.getElementById('min').addEventListener('keydown', (e) => {
+    if(e.key === 'Enter') {
+        rng();
+    }
+})
+document.getElementById('max').addEventListener('keydown', (e) => {
+    if(e.key === 'Enter') {
+        rng();
+    }
+})
