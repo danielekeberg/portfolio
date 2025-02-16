f6dcbcb23a0dbca2ed4892772f77fd66dfe88f07
Daniel Lehre Ekeberg <154937462+danielekeberg@users.noreply.github.com>
2025-02-16 19:09:27 +0100

    Testing abilities

---
+++ lod/js/script.js
@@ -0,0 +1,149 @@
+const garenHealthBar = document.getElementById('health-bar');
+const garenHealthPoints = document.getElementById('hp');
+
+let garenHP = 100;
+
+let qAd = 7;
+let wAd = 2;
+let eAd = 5;
+let rAd = 14;
+
+let qCd = 12;
+let isQCooldown = false;
+let wCd = 7;
+let isWCooldown = false;
+let eCd = 17;
+let isECooldown = false;
+let rCd = 72;
+let isRCooldown = false;
+
+function resetGarenHP() {
+    garenHP = 100;
+}
+
+function garen() {
+    garenHealthBar.style.width = `${garenHP}px`;
+    garenHealthPoints.textContent = `${garenHP} HP`;
+    if(garenHP <= 0) {
+        alert('garen noob');
+        resetGarenHP();
+        garen();
+    }
+}
+
+garen();
+
+function q() {
+    if(isQCooldown) return;
+    
+    console.log(qCd);
+    garenHP -= qAd;
+    garen();
+
+    isQCooldown = true;
+    document.getElementById('q').textContent = qCd;
+    document.getElementById('q').className = 'cd';
+    const cooldown = setInterval(() => {
+        qCd--;
+        document.getElementById('q').textContent = qCd;
+        if(qCd <= 0) {
+            clearInterval(cooldown)
+            qCd = 12;
+            isQCooldown = false;
+            document.getElementById('q').textContent = 'Q';
+            document.getElementById('q').className = '';
+        }
+    }, 1000);
+}
+function w() {
+    if(isWCooldown) return;
+
+    console.log(wCd);
+    garenHP -= wAd;
+    garen();
+
+    isWCooldown = true;
+    document.getElementById('w').textContent = wCd;
+    document.getElementById('w').className = 'cd';
+    const cooldown = setInterval(() => {
+        wCd--;
+        document.getElementById('w').textContent = wCd;
+        if(wCd <= 0) {
+            clearInterval(cooldown)
+            wCd = 7;
+            isWCooldown = false;
+            document.getElementById('w').textContent = 'W';
+            document.getElementById('w').className = '';
+        }
+    }, 1000);
+}
+function eAbility() {
+    if(isECooldown) return;
+
+    console.log(eCd);
+    garenHP -= eAd;
+    garen();
+
+    isECooldown = true;
+    document.getElementById('e').textContent = eCd;
+    document.getElementById('e').className = 'cd';
+    const cooldown = setInterval(() => {
+        eCd--;
+        document.getElementById('e').textContent = eCd;
+        if(eCd <= 0) {
+            clearInterval(cooldown)
+            eCd = 12;
+            isECooldown = false;
+            document.getElementById('e').textContent = 'E';
+            document.getElementById('e').className = '';
+        }
+    }, 1000);
+}
+function r() {
+    if(isRCooldown) return;
+
+    console.log(rCd);
+    garenHP -= rAd;
+    garen();
+
+    isRCooldown = true;
+    document.getElementById('r').textContent = rCd;
+    document.getElementById('r').className = 'cd';
+    const cooldown = setInterval(() => {
+        rCd--;
+        document.getElementById('r').textContent = rCd;
+        if(rCd <= 0) {
+            clearInterval(cooldown)
+            rCd = 12;
+            isRCooldown = false;
+            document.getElementById('r').textContent = 'R';
+            document.getElementById('r').className = '';
+        }
+    }, 1000);
+}
+
+document.getElementById('q').addEventListener('click', q);
+document.addEventListener('keydown', (e) => {
+    if(e.key === 'q') {
+        console.log('asdasd')
+        q();
+    }
+})
+document.addEventListener('keydown', (e) => {
+    if(e.key === 'w') {
+        console.log('asdasd')
+        w();
+    }
+})
+document.addEventListener('keydown', (e) => {
+    if(e.key === 'e') {
+        console.log('eee')
+        eAbility();
+    }
+})
+document.addEventListener('keydown', (e) => {
+    if(e.key === 'r') {
+        console.log('asdasd')
+        r();
+    }
+})
