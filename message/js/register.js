59ac7b3f409e344b93ceffb6f3dfcd7e575ab3f2
Daniel Lehre Ekeberg <danieleke@live.no>
2025-07-06 03:10:24 +0200

    Update register.js

--- message/js/register.js
+++ message/js/register.js
@@ -1,64 +1,68 @@
 import { userAPI } from "./config.js";
 
 async function register() {
-    const username = document.getElementById('usernameInput').value;
-    const password = document.getElementById('passwordInput').value;
-    const id = await getTotal();
-
     try {
-        const res = await fetch(`${userAPI}/search?username=${username}`);
+        const username = document.getElementById('usernameInput').value;
+        const display = document.getElementById('displayNameInput').value;
+        const password = document.getElementById('passwordInput').value;
+
+        const res = await fetch(`${userAPI}`);
         const data = await res.json();
-        if(data.length > 0) {
-            console.warn('Username already exists.');
+
+        const findUser = data.find(user => user.username === username);
+        if(findUser) {
+            console.log('Username already exists');
             return;
         }
-        const postRes = await fetch(userAPI, {
+
+        const allUsers = await totalUsers();
+
+        const response = await fetch(userAPI, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
             },
             body: JSON.stringify({
-                data: {
-                    username: username,
-                    password: password,
-                    id: id,
-                    avatar: 'https://static.vecteezy.com/system/resources/previews/009/292/244/non_2x/default-avatar-icon-of-social-media-user-vector.jpg'
-                }
+                id: allUsers,
+                username: username,
+                display: display,
+                password: password
             })
-        });
-        window.location.href = '../login/';
+        })
+
+        if(response.ok) {
+            window.location.href = '../login/';
+        } else {
+            console.log('error');
+        }
     } catch(error) {
         console.error(error);
     }
 }
 
-async function getTotal() {
+async function totalUsers() {
     try {
         const res = await fetch(userAPI);
         const data = await res.json();
-        return data.length + 1;
+        return data.length - 1;
     } catch(error) {
         console.error(error);
     }
 }
 
-document.body.addEventListener('keydown', (e) => {
+document.getElementById('signUp').addEventListener('click', register);
+document.getElementById('passwordInput').addEventListener('keydown', (e) => {
     if(e.key === 'Enter') {
         register();
     }
 })
-document.body.addEventListener('keydown', (e) => {
-    if(e.key === 'S') {
-        test();
+document.getElementById('displayNameInput').addEventListener('keydown', (e) => {
+    if(e.key === 'Enter') {
+        register();
     }
 })
-
-async function test() {
-    try {
-        const res = await fetch(`${userAPI}/search?username=daniel`);
-        const data = await res.json();
-        console.log(data);
-    } catch(error) {
-        console.error(error);
+document.getElementById('usernameInput').addEventListener('keydown', (e) => {
+    if(e.key === 'Enter') {
+        register();
     }
-}
+})
