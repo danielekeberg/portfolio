b59a9ee160d74f516a2d33f9e3a06108ac31355b
Daniel Lehre Ekeberg <danieleke@live.no>
2025-08-10 22:20:10 +0200

    Update userid.js

--- runtracker/js/userid.js
+++ runtracker/js/userid.js
@@ -9,6 +9,9 @@ if(!uid) {
         <h3 style="text-align: center;">You need a User ID to continue!</h3>
         <div class="userid-forms">
             <input type="text" placeholder="123456" id="uidInput">
+            <button id="continue">Continue</button>
+        </div>
+        <div class="new-form">
             <button id="generateUid">Generate New UID</button>
         </div>
     </div>
@@ -18,8 +21,7 @@ if(!uid) {
         document.getElementById('uidInput').addEventListener('keydown', (e) => {
             if(e.key === 'Enter') {
                 localStorage.setItem('userid', document.getElementById('uidInput').value);
-                document.querySelector('.overlay').remove();
-                console.log('value set');
+                window.location.reload();
             }
         })
     });
@@ -30,7 +32,7 @@ if(!uid) {
             const newuid = String(random).padStart(6, '0');
             const finisheduid = Number(newuid);
             localStorage.setItem('userid', finisheduid);
-            document.querySelector('.overlay').remove();
+            window.location.reload();
         })
     })
 
